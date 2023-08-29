21cefea785cac39bac5034cf2f3dacd57b355af4
Mass Jack <mass.jack4@gmail.com>
2023-08-29 18:14:10 +0200

    fix: Add color on theme dark & Add Switch

--- src/components/HeaderDropdown.jsx
+++ src/components/HeaderDropdown.jsx
@@ -3,14 +3,24 @@ import {useSelector} from "react-redux";
 import boardIcon from '../assets/icon-board.svg';
 import lightIcon from '../assets/icon-light-theme.svg';
 import darkIcon from '../assets/icon-dark-theme.svg';
-
+import {Switch} from "@headlessui/react";
+import useDarkMode from "../Hooks/useDarkMode.js";
+import {useState} from "react";
 
 
 function HeaderDropdown({setOpenDropdown}) {
+    const [colorTheme, setTheme] = useDarkMode()
+    const [darkSide, setDarkSide] = useState(
+        colorTheme === 'light'
+    )
+
+    const toggleDarkMode = (checked) => {
+        setTheme(colorTheme)
+        setDarkSide(checked)
+    }
 
     const boards = useSelector((state) => state.boards)
 
-    console.log('boards =', boards)
     return(
         <div className=" py-10 px-6 absolute left-0 right-0 bottom-[-100vh] top-16 bg-[#00000080]" onClick={
             (e) => {
@@ -31,7 +41,7 @@ function HeaderDropdown({setOpenDropdown}) {
                 <div>
                     {boards.map((board, index) => (
                         <div
-                        className={`flex items-baseline space-x-2 px-5 py-4 ${board.isActive && 'bg-[#DD7878] rounded-r-full text-white mr-8'}`}
+                        className={`flex items-baseline dark:text-white space-x-2 px-5 py-4 ${board.isActive && 'bg-[#DD7878] rounded-r-full text-white mr-8'}`}
                         key={index}
                         >
                             <img src={boardIcon} className='h-4' alt='boards' />
@@ -49,7 +59,16 @@ function HeaderDropdown({setOpenDropdown}) {
                     <div className='mx-2 p-4 space-x-2 bg-slate-100 dark:bg-[#20212x] flex justify-center items-center rounded-lg'>
                         <img src={lightIcon} alt=""/>
 
-                        {/* Switch */}
+                        <Switch
+                            checked={darkSide}
+                            onChange={toggleDarkMode}
+                        className={`${darkSide ? 'bg-[#635fc71]' : 'bg-gray-200'} relative inline-flex h6 w-11 items-center rounded-full`}>
+
+                            <span
+                            className={`${darkSide ? 'translate-x-6' : 'translate-x-1'} inline-block h-4 w-4 transform rounded-full bg-white transition`} />
+
+
+                        </Switch>
 
                         <img src={darkIcon} alt=""/>
                     </div>
